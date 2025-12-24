f92f566d8c3edb04350434d49d0141836d0057e9
Matteo Toutain <matteotoutain@gmail.com>
2025-12-24 14:16:10 +0100

    Update build_proba_od_for_entities.py

--- scripts/build_proba_od_for_entities.py
+++ scripts/build_proba_od_for_entities.py
@@ -1,7 +1,13 @@
 # scripts/build_proba_od_for_entities.py
+import sys
+from pathlib import Path
+
+# Ajoute la racine du repo au PYTHONPATH
+ROOT_DIR = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(ROOT_DIR))
+
 from __future__ import annotations
 
-from pathlib import Path
 from typing import List
 
 import pandas as pd
